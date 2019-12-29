2121e1feb138982d5abfc0279555e873d14625aa
slzander <staceyzander@gmail.com>
2019-12-29 15:18:07 -0700

    configures map component with leaflet map so that it shows up on page centered around north and south america

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import * as d3 from 'd3';
+import data from './sandpiper-test.csv'
 import Map from './components/Map'
 
 export default class App extends React.Component {
+  state = {
+    birdData: {}
+  }
+  
+  componentDidMount(){
+    d3.csv(data)
+      .then(data => console.log(data))
+  }
+
   render(){
     return(
       <div className="App">
-        <header className="App-header">
-        </header>
+        <header className="App-header"></header>
+        <Map
+          birdData={this.state.birdData} />
       </div>
     )
     }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import styled from 'styled-components'
 
-export default function Map() {
-    return(
-        console.log('map is loaded')
-    )
+const Wrapper = styled.div`
+    width: ${props => props.width};
+    height: ${props => props.height};
+`;
+
+export default class Map extends React.Component {
+    
+    componentDidMount(){
+        const map = L.map('map', {
+            center: [0, -80],
+            zoom: 4
+        })
+
+        L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+            detectRetina: true,
+            maxZoom: 10,
+            attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+        }).addTo(map)
+    }
+
+    render(){
+        return(
+            <Wrapper width='720px' height='1280px' id='map' />
+        )
+    }
 }
